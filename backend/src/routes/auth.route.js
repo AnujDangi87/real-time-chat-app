1bc2d9c45580ca208c5dc65df1fb45755cfaadda
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-04 00:43:34 +0530

    Added controllers for signup, login, logout api/auth routes to improve modularity, reusability

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,17 +1,13 @@
 import express from "express";
+import {signup, login, logout} from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("signup route");
-});
+//Post as the sender will send sensitive data to web
+router.post("/signup", signup);
 
-router.get("/login", (req, res) => {
-    res.send("login route");
-});
+router.post("/login", login);
 
-router.get("/logout", (req, res) => {
-    res.send("logout route");
-});
+router.post("/logout", logout);
 
 export default router;
