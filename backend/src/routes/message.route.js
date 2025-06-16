2211eb72639c23ea94296d72f00eaee0a6ce93a7
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-16 23:57:20 +0530

    updating getMessage endpoint on message route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 
 import { protectRoute } from "../middleware/auth.middleware";
-import { getUsersForSidebar } from "../controllers/message.controller";
+import { getUsersForSidebar, getMessages } from "../controllers/message.controller";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
 
 export default router;
