05a006a382ed53a02017da5327451b748f29dd51
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-17 00:25:18 +0530

    added sendMessage endpoint on message route

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js";
 import Message from "../models/message.model.js";
 import User from "../models/user.model.js";
 
@@ -36,4 +37,39 @@ export const getMessages = async (req, res) => {
         console.log("Error in getMessage controller", error.message);
         res.status(500).josn({error: "Internal server error"});
     }
-}
+};
+
+export const sendMessage = async (req, res) => {
+    try
+    {
+        const { text, image } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image)
+        {
+            //Upload base64 image to cloudinary
+            const uploadResponse = await cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url;
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl,
+        });
+
+        await newMessage.save();
+
+        //todo : realtime functionality goes here => socket.io
+
+        res.status(201).json(newMessage);
+    }
+    catch(error)
+    {
+        console.log("Error in getMessage controller:", error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+};
