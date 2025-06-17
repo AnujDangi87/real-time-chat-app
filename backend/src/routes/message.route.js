ceb138d03f59e0ccfeb79c81106c8a97fb25c427
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-17 18:46:19 +0530

    fixing .js bugs in import statments in message route

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,7 +1,7 @@
 import express from "express";
 
-import { protectRoute } from "../middleware/auth.middleware";
-import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getUsersForSidebar, getMessages, sendMessage } from "../controllers/message.controller.js";
 
 const router = express.Router();
 
