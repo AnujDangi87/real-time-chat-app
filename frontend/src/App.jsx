6aa5870bfa5173347dfbbbe745451fe2e3b186f2
vsaa-org <23mcce16@uohyd.ac.in>
2025-06-18 16:46:03 +0530

    creating Components and Pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      Hello World
+    <div>
+      <Navbar/>
+
+      <Routes>
+        
+        <Route path="/" element={<HomePage/>}></Route>
+        <Route path="/signup" element={<SignUpPage/>}></Route>
+        <Route path="/login" element={<LoginPage/>}></Route>
+        <Route path="/settings" element={<SettingsPage/>}></Route>
+        <Route path="/profile" element={<ProfilePage/>}></Route>
+
+      </Routes>
     </div>
   )
 }
